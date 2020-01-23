93d728fcc0dd5538460531fabf76a01449fbe374
Terra1 <terrariola@gmail.com>
2020-01-23 12:02:09 +0100

    Create unsh.c
    The glorious first commit.

---
+++ src/unsh.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+char termchar = ]
+
+void inloop() {
+	int ready()
+	do {
+		printf(termchar);
+		inputchar = getchar( );
+	} while (ready)
+}
+
+void exec() {
+	
+}
+
+int main {
+	inloop();
+	exec();
+}
