4d96f1ede2c0e91b76595b7f11be80540840085b
Terra1 <terrariola@gmail.com>
2020-01-24 09:17:52 +0100

    First functional release?

--- src/unsh.c
+++ src/unsh.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
-char termchar = "]";
+#include <unistd.h>
 
-void inloop() {
+char termchar = "]";
+int pid = pid_t;
+
+void inloop() { //The actual shell function.
 	int ready()
 	do {
 		printf(termchar);
-		inputchar = getchar( );
+		const char *inputchar[] = getchar( );
+		printf("Program Arguments:")
+		char *const args[] = getchar( );
+		fork();
 	} while (ready)
 }
 
-void exec() {
-	
+void exec_cmd() { //Function for executing programs.
+	exec(inputchar, args[]); //Replaces the existing program with the one previously set in inputchar.
 }
 
 int main {
-	inloop();
-	exec();
+	if (pid != 0) { //Checks if the PID is zero.
+		inloop();
+	}
+	else { //If the PID not zero, assume that this program is a duplicate of the existing shell.
+		exec_cmd();
+	}
 }
