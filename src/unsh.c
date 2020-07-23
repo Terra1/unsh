906b43577a86fc79e1f00a375ce16cd00f7fb91c
Terra1 <terrariola@gmail.com>
2020-07-23 14:44:57 +0200

    Total overhaul.

--- src/unsh.c
+++ src/unsh.c
@@ -1,24 +1,33 @@
 #include <stdio.h>
 #include <unistd.h>
 
-char termchar = "]";
-pid_t pid = getpid();
+#define MAX_PROGRAM_NAME_LENGTH 100
+#define MAX_ARGS_LENGTH 100
+
+const char termchar = '>';
+pid_t pid;
+
+char programName[MAX_PROGRAM_NAME_LENGTH];
+char* args[MAX_ARGS_LENGTH];
 
 void inloop() { //Runs the shell.
 	for(;;) {
-		printf(termchar);
-		const char *inputchar[] = getchar( );
-		printf("Program Arguments:")
-		char *const args[] = getchar( );
+		printf(&termchar);
+		fgets(programName, MAX_PROGRAM_NAME_LENGTH, stdin);
+		getchar();
+		printf("Program Arguments:");
+		fgets(*args, MAX_ARGS_LENGTH, stdin);
+		getchar();
 		fork();
 	}
 }
 
 void exec_cmd() { //Function for executing programs.
-	execvp(inputchar, args[]); //Replaces the existing program with the one previously set in inputchar.
+	execvp(programName,args); //Replaces the existing program with the one previously set in inputchar.
 }
 
 int main() {
+	pid = getpid();
 	if (pid != 0) { //Checks if the PID is not zero.
 		inloop(); //If the PID is not zero, run the shell using the inloop() function, starting at line 7.
 	}
